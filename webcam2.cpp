a71d3690491eab8013f24e9e1de3d941c8213b09
c2h2 <yiling.cao@gmail.com>
2015-03-02 23:37:27 +0800

    canny edge for webcam2.cpp

--- webcam2.cpp
+++ webcam2.cpp
@@ -14,8 +14,15 @@ int main() {
   	int i=0; //number of frames
   	char str[64];
   	char buf[5];
+
+	int edgeThresh = 1;
+	int lowThreshold;
+	int const max_lowThreshold = 100;
+	int ratio = 3;
+	int kernel_size = 3;
  
 	cv::Mat frame;
+	cv::Mat cap;
 	cv::Mat matProcessed;
 	cv::VideoCapture capWebcam;
 
@@ -32,27 +39,25 @@ int main() {
 	cv::resizeWindow(WINDOW_NAME, 480, 320);
 	cv::resizeWindow(WINDOW_NAME2, 480, 320);
 
+	cv::Mat frame_gray;
+	cv::Mat detected_edges;
+	
 	while(true){
 		
-		capWebcam.read(frame);
-		cv::resize(frame, frame, cv::Size(640, 360), 0, 0, cv::INTER_CUBIC);
-		cv::inRange(frame, cv::Scalar(100,0,0), cv::Scalar(255,100,100), matProcessed);  //scalar is BRG not RGB
-		cv::GaussianBlur(matProcessed, matProcessed, cv::Size(9,9), 1.5);
+		capWebcam.read(cap);
+		cv::resize(cap, frame, cv::Size(640, 360), 0, 0, cv::INTER_CUBIC);
+		//cv::inRange(frame, cv::Scalar(100,0,0), cv::Scalar(255,100,100), matProcessed);  //scalar is BGR not RGB
+		//cv::GaussianBlur(matProcessed, matProcessed, cv::Size(9,9), 1.5);
 		//cv::HoughCircles(matProcessed, vecCircles, CV_HOUGH_GRADIENT, 2, matProcessed.rows / 4, 100, 50, 10, 400);
-
+      		cv::cvtColor( frame, frame_gray, CV_BGR2GRAY );
+		cv::blur(frame_gray, detected_edges, cv::Size(3,3) );
+		cv::Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
 		//for(itrCircles = vecCircles.begin(); itrCircles != vecCircles.end(); itrCircles++){
 		//	std::cout <<"x="<<  itrCircles[0] << "\n"; // << ", y=" <<  itrCircles[1] << ", z=" <<itrCircles[2] << "\n";
 		//}
 
 		imshow(WINDOW_NAME, frame);
-		imshow(WINDOW_NAME2, matProcessed);
-		strcpy(str, BASENAME);
-		sprintf(buf,"%05d",i);
-		strcat(str, buf);
-		strcat(str, IMGFORMAT);
-
-		//imwrite(str , frame);
-		//i++;
+		imshow(WINDOW_NAME2, detected_edges);
 	}
 	return 0;
 }
